bfeba47d4598f384b821e09eaa89833f5646e666
Carlos Rojo <crojo@ciatej.mx>
2021-12-29 12:44:49 -0600

    encapsulation on Patiens or users

--- src/main/java/com/crojo/consultorio/domain/Patients.java
+++ src/main/java/com/crojo/consultorio/domain/Patients.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public class Patients implements IPerson {
 	
-	Long id;
-	String name;
-	String lastName;
-	String surName;
-	Integer age;
-	String gender;
-	String email;
-	String phoneNumber;
+	private Long id;
+	private String name;
+	private String lastName;
+	private String surName;
+	private Integer age;
+	private String gender;
+	private String email;
+	private String phoneNumber;
 	Date crationTime;
 	Date modificationTime;
 	Long userModifier;
@@ -21,6 +21,102 @@ public class Patients implements IPerson {
 	public String tostrin() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getCrationTime() {
+		return crationTime;
+	}
+
+	public void setCrationTime(Date crationTime) {
+		this.crationTime = crationTime;
+	}
+
+	public Date getModificationTime() {
+		return modificationTime;
+	}
+
+	public void setModificationTime(Date modificationTime) {
+		this.modificationTime = modificationTime;
+	}
+
+	public Long getUserModifier() {
+		return userModifier;
+	}
+
+	public void setUserModifier(Long userModifier) {
+		this.userModifier = userModifier;
+	}
+
+	public Long getUserCreator() {
+		return userCreator;
+	}
+
+	public void setUserCreator(Long userCreator) {
+		this.userCreator = userCreator;
 	}
 	
 
--- src/main/java/com/crojo/consultorio/domain/Users.java
+++ src/main/java/com/crojo/consultorio/domain/Users.java
@@ -3,23 +3,133 @@ package com.crojo.consultorio.domain;
 import java.util.Date;
 
 public class Users implements IPerson {
-	String name;
-	String lastName;
-	String surName;
-	String email;
-	String role;
-	String userName;
-	String password;
-	Date crationTime;
-	Date modificationTime;
-	Long userModifier;
-	Long userCreator;
+	private String name;
+	private String lastName;
+	private String surName;
+	private String email;
+	private String role;
+	private String userName;
+	private String password;
+	private Date crationTime;
+	private Date modificationTime;
+	private Long userModifier;
+	private Long userCreator;
 	
 	
 	@Override
 	public String tostrin() {
 		
 		return  name + " " + lastName + " " + surName ;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getSurName() {
+		return surName;
+	}
+
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public Date getCrationTime() {
+		return crationTime;
+	}
+
+
+	public void setCrationTime(Date crationTime) {
+		this.crationTime = crationTime;
+	}
+
+
+	public Date getModificationTime() {
+		return modificationTime;
+	}
+
+
+	public void setModificationTime(Date modificationTime) {
+		this.modificationTime = modificationTime;
+	}
+
+
+	public Long getUserModifier() {
+		return userModifier;
+	}
+
+
+	public void setUserModifier(Long userModifier) {
+		this.userModifier = userModifier;
+	}
+
+
+	public Long getUserCreator() {
+		return userCreator;
+	}
+
+
+	public void setUserCreator(Long userCreator) {
+		this.userCreator = userCreator;
 	}
 
 }
